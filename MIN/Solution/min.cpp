125298ee37b77a477d37d8aab36f23ddb5f3cb2d
Huy1902 <nguyenhuy9185@gmail.com>
2021-11-16 22:27:15 +0700

    BT 1 -> 5

--- MIN/Solution/min.cpp
+++ MIN/Solution/min.cpp
@@ -1,104 +1,63 @@
+
 #include <bits/stdc++.h>
 
-std::ifstream cin("min.inp");
-std::ofstream cout("min.out");
+using namespace std;
+long long n;
 
-void create5Digit(long long n, int k)
-{
-    //convert n to string s
-    std::string s = "";
-    int temp = n;
-    while (temp > 0)
-    {
-        s = s + char(temp % 10 + '0');
-        temp /= 10;
-    }
-    // end convert
-
-    //special case
-    if (s.size() <= k)
-    {
-        long long result = 0;
-        while (k > 0)
-        {
-            result = result * 10 + 5;
-            --k;
-        }
-        if (result > n)
-        {
-            cout << result;
-            return;
-        }
-        else
-        {
-            cout << 1 << result;
-            return;
-        }
-    }
-    //end case
-
-    //create new number
-    int range = s.size();
-    int n_digit_5 = 0;
-    for (int i = 0; i < range; ++i)
-    {
-        ++n_digit_5;
-    }
-
-    std::string compare = s;
-    int n_digit_need = k - n_digit_5;
-    int i;
-    for (i = range - 1; n_digit_need > 0; --i)
-    {
-        if (s[i] == '5')
-        {
-            ++n_digit_need;
-        }
-        s[i] = '5';
-        --n_digit_need;
-    }
-    //end creation;
-
-    //check result;
-    ++i;
-    if (compare[i] < s[i])
-    {
-        cout << s;
-        return;
-    }
-    //end check;
-
-    //optimize result
-    --i;
-    while (s[i] == '5')
-    {
-        --i;
-    }
-    if (s[i] == '4')
-    {
-        s[i] = '5';
-        s[i + 1] = '0';
-        cout << s;
-        return;
-    }
-
-    long long result = 0;
-    for (int j = 0; j <= i; ++j)
-    {
-        result = result * 10 + s[j] - '0';
-    }
-    cout << result + 1 << s.substr(i, range - i);
-
-    return;
-}
-
+int k, a[100], nho, luu, k1, nho1, c;
 int main()
 {
-    long long n;
-    int k;
+    freopen("MIN.inp", "r", stdin);
+    freopen("MIN.out", "w", stdout);
+
     cin >> n >> k;
+    nho = 1;
+    k1 = k;
+    while (n > 0)
+    {
+        a[nho] = n % 10;
+        n = n / 10;
+        nho++;
+    }
+    if (k <= nho)
+    {
+        for (int i = k + 1; i < nho; i++)
+            if (a[i] == 5)
+                k--;
+        for (int i = k1; i >= k + 1; i--)
+            if (a[i] == 5)
+                k--;
 
-    create5Digit(n, k);
+        c = 1;
+        for (int i = 1; i <= k; i++)
+            a[i] = 5;
+        {
+            for (int i = k + 1; i <= nho; i++)
+                if (a[i] + nho1 + c > 9)
+                {
+                    a[i] = 0;
+                    nho1 = 1;
+                    c = 0;
+                }
+                else
+                {
+                    a[i] = a[i] + 1;
+                    if (a[i] == 5)
+                        a[k] = 0;
+                    nho1 = 0;
+                    break;
+                }
+        }
 
+        if (a[nho] == 0)
+            nho = nho - 1;
+        for (int i = nho; i >= 1; i--)
+            cout << a[i];
+    }
+    else
+    {
+        for (int i = 1; i <= k; i++)
+            cout << 5;
+    }
     return 0;
 }
